0b66a6aaec1aa70cb6b13ddd4ad85ac2d5e4281d
say10s <nikodemmajzner@gmail.com>
2023-11-27 20:26:42 +0100

    DOne.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+// app.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bank';
+  balance: number = 0;
+  incomeList: { amount: number; title: string }[] = [];
+  expensesList: { amount: number; title: string }[] = [];
+  transactionAmount: number = 0;
+  transactionTitle: string = '';
+
+  addTransaction(amount: number, title: string): void {
+    if (amount >= 0) {
+      this.balance += amount;
+      this.incomeList.push({ amount, title });
+    } else {
+      this.balance += amount;
+      this.expensesList.push({ amount: Math.abs(amount), title });
+    }
+  }
 }
